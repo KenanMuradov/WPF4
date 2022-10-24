1f4eb062cc9cff21c426ecb2648869ae8da87aab
Mura_mw07@itstep.edu.az <Mura_mw07@itstep.edu.az>
2022-10-24 17:26:37 +0400

    Add project files.

---
+++ WPF4/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF4;
+
+public partial class MainWindow : Window
+{
+    public List<User> Users { get; set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        DataContext = this;
+
+        Users = new Faker<User>()
+            .RuleFor(u => u.Firstname, f => f.Person.FirstName)
+            .RuleFor(u => u.Lastname, f => f.Person.LastName)
+            .Generate(15);
+
+
+    }
+}
